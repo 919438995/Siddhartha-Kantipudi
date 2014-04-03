bb29859be9a022c52138d15c5e923c08f7a6efd2
siddhuKantipudi <siddhu.kantipudi@gmail.com>
2014-04-03 13:43:53 -0500

    adding sout statement- Siddhartha Kantipudi

---
+++ src/myapplication/MyApplication.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myapplication;
+
+/**
+ *
+ * @author S516529
+ */
+public class MyApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("This project shows how to create repositiory in git hub ");
+    }
+    
+}
